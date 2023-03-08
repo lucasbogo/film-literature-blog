c26e6e039af422649acba2d03677bc3a48086ea2
lucasbogo <lucasbogo93@gmail>
2023-03-08 19:57:35 -0300

    refacotored folder names

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/pages/index.html');
+    res.sendFile(__dirname + '/views/index.html');
     });
 
 app.listen(4000, () => {
