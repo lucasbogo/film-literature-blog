977f238d72aff8c638db33906ccc9c26bb3af812
lucasbogo <lucasbogo93@gmail>
2023-03-08 20:56:47 -0300

    added routes to index.js

--- index.js
+++ index.js
@@ -7,6 +7,18 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html');
     });
 
+app.get('/about', (req, res) => {
+    res.sendFile(__dirname + '/views/about.html');
+    });
+
+app.get('/contact', (req, res) => {
+    res.sendFile(__dirname + '/views/contact.html');
+    });
+
+app.get('/post', (req, res) => {
+    res.sendFile(__dirname + '/views/post.html');
+    });
+
 app.listen(4000, () => {
     console.log('Server is running on port 4000');
     });
