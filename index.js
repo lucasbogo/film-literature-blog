a40d1c056bb1674866e32705c8a44c4fa0cb680b
lucasbogo <lucasbogo93@gmail>
2023-03-08 19:40:18 -0300

    bootstrap theme added

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const app = express();
 
 app.use(express.static('public'));
 
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/pages/index.html');
+    });
+
 app.listen(4000, () => {
     console.log('Server is running on port 4000');
     });
