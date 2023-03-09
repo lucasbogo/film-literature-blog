2bf807e6937b3ab8204de19be1ead99a2ca31a57
lucasbogo <lucasbogo93@gmail>
2023-03-08 23:18:40 -0300

    models for Post

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const path = require('path');
-
 const express = require('express');
-
 const expressEdge = require('express-edge');
+const mongoose = require('mongoose');
 
-const app = express();
+const app = new express();
+
+mongoose.connect('mongodb://localhost:27017/node-js-blog', { useNewUrlParser: true })
+    .then(() => 'You are now connected to Mongo!')
+    .catch(err => console.error('Something went wrong', err))
 
 app.use(express.static('public'));
 app.use(expressEdge.engine);
