0f2ebd99438aff4787c05c5735af2f6303429f85
lucasbogo <lucasbogo93@gmail>
2023-03-08 21:56:02 -0300

    routes for static pages working fine

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
+const path = require('path');
+
 const express = require('express');
+
 const app = express();
 
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/views/index.html');
+    res.sendFile(__dirname + '/views/home.html');
     });
 
 app.get('/about', (req, res) => {
